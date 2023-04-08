429f56acf68edb7cccc9359aa6034a93b5d9e8f8
daisukeokamoto1023 <okadaih11023@gmail.com>
2023-04-08 16:57:59 +0000

    first commit

---
+++ main.py
@@ -0,0 +1,57 @@
+"""Python file to serve as the frontend"""
+import streamlit as st
+from streamlit_chat import message
+
+from langchain.chains import ConversationChain
+from langchain.llms import OpenAI
+from dotenv import load_dotenv
+
+load_dotenv()
+
+st.set_page_config(page_title="LangChain Demo", page_icon=":robot:")
+@st.cache_resource
+def load_chain() -> ConversationChain:
+    """Logic for loading the chain you want to use should go here."""
+    llm = OpenAI(temperature=0)
+    chain = ConversationChain(llm=llm)
+    return chain
+
+chain: ConversationChain = load_chain()
+
+# From here down is all the StreamLit UI.
+
+st.header("LangChain Demo")
+
+if "generated" not in st.session_state:
+    st.session_state["generated"] = []
+
+if "past" not in st.session_state:
+    st.session_state["past"] = []
+
+
+if "input_text" not in st.session_state:
+    st.session_state["input_text"] = ""
+
+
+def get_text() -> str:
+    input_text: str = st.text_input("You: ", st.session_state["input_text"], key="input_text", placeholder="please type here")
+    return input_text
+
+def clear_text():
+    st.session_state["input_text"] = ""
+
+with st.form(key="form", clear_on_submit=True):
+    user_input: str = st.text_area("You: ", "", key="input_text", placeholder="please type here")
+    submit: bool = st.form_submit_button("Submit")
+
+if submit:
+    output: str = chain.run(input=user_input)
+
+    st.session_state.past.append(user_input)
+    st.session_state.generated.append(output)
+
+if st.session_state["generated"]:
+
+    for i in range(len(st.session_state["generated"]) - 1, -1, -1):
+        message(st.session_state["generated"][i], key=str(i))
+        message(st.session_state["past"][i], is_user=True, key=str(i) + "_user")
